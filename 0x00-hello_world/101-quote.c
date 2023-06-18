067b6386311e01ced7b962bcaa24140b75da514e
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-17 20:47:45 -0700

    prints a word followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	const char message[] = "and that piece of art is used\" - Dora Korpar, 2015-10-19\n";
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(2, message, strlen(message));
+	write(2, message, strlen(message));
 	return (1);
 }
