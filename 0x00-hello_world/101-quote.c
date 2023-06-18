77720e3a16662a125373da21f2352c43358b8b18
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-17 20:37:44 -0700

    prints a word followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	const char message[] = "and that piece of art is used\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+	return (1);
+}
