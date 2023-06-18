4a2400a573de9e1b8c8a8873c9276144ecfba1cd
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-18 05:11:18 -0700

    prints a particular word and followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/*initializing strings*/
-	char str1[] = "\"Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 	/*writing to stdout*/
 	puts(str1);
 	return (0);
