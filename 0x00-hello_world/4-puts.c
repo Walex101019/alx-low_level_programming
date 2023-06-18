a0b300fd57effe60775da274b215f681ce20be75
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-17 19:16:08 -0700

    Prints a particular word

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+  /*initializing strings*/
+  char str1[] = "Programming is like building a multilingual puzzle";
+  /*writing to stdout*/
+  puts(str1);
+  return (0);
+}
