99fec37d204bb5a7a4a3f51c802dbd682bd9387b
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-19 18:33:27 -0700

    prints the last digit of the vaiable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,8 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, abs(n % 10));
-	lastDigit = abs(n % 10);
+	printf("Last digit of %d is %d ", n, (n % 10));
+	lastDigit = (n % 10);
 	if (lastDigit > 5)
 		printf("and is greater than 5\n");
 	else if (lastDigit == 0)
