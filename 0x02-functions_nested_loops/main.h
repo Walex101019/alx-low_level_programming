bbd3d0acd3f2d1f7092cfbf0944a21c49dca0c28
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-21 10:04:36 -0700

    prints alphabet then follow by new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@
  * print_alphabet - Entry point.
  * void print_alphabet void: specifies the function doesn't return a value.
  */
-void print_alphabet_x10(void);
+void print_alphabet(void);
 int _putchar(char c);
 
 #endif /* MAIN_H */
