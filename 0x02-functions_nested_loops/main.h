6fd637d3ddb5834098626ccf5009da452b78c207
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-21 07:50:03 -0700

    Write a function that prints the alphabe in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * Writes a character to the standard output.
- *
- * param char c: The charcter to be written and printed.
- */
- int _putchar(char c);
+
+ void print_alphabet(void);
 
 #endif /* MAIN_H */
