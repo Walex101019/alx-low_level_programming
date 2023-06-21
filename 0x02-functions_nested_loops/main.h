8d3f8b0a9c870c754d9d84a48aa2411021d2dc16
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-21 07:23:45 -0700

    prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Writes a character to the standard output.
+ *
+ * param char c: The charcter to be written and printed.
+ */
+ int _putchar(char c);
+
+#endif /* MAIN_H */
