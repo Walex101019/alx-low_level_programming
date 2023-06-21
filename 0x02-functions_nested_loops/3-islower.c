87e17a36318fcea26b050e3e9df55e80d4c78851
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-21 16:09:21 -0700

    Checks if and alphabet is in lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 or 1 (Success)
+ */
+int main(void)
+{
+	char ch = 'c';
+
+	if islower(ch);
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,6 @@
  */
 void print_alphabet(void);
 int _putchar(char c);
+int _islower(int c);
 
 #endif /* MAIN_H */
