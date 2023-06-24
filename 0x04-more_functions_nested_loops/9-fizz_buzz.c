5d7cfa338c78df5368abf88d8d6fbc4a462ab8fe
Walex101019 <ibietanwasiu94@gmail.com>
2023-06-23 20:08:20 -0700

    Prints FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - Entry point
+ * main - Prints a fixxbzz program
  *
  * Return: Returns 0
  *
@@ -9,23 +9,23 @@ int main(void)
 {
 	int x;
 
-	for (x >= 1; x <= 100; x++)
-    	{
-		if (x % 3 == 0)
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("Fizz\t");
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz\t");
-		}
-		else if (x % 3 == 0 && x % 5 == 0)
-		{
-			printf("FIzzBuzz\t");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d\t", x);
+			printf("%d ", x);
 		}
 	}
 	printf('\n');
