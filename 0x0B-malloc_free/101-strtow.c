6b4f8d7a81d746b94867734b790f665374be1cf3
Walex101019 <ibietanwasiu94@gmail.com>
2023-07-11 18:53:51 -0700

    splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,75 +1,72 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 /**
  * **strtow - Entry point
- *
  * @str: parameter passed
- *
- * Return: return words
+ * Return: return word
  */
 char **strtow(char *str)
 {
 	if (str == NULL || *str == '\0')
 	{
-		return NULL;
+		return (NULL);
 	}
 	int word_count = 0;
 	int i = 0;
 	int j;
 	int length = strlen(str);
-	while (i < length)
+
+	while (str[i] != '\0')
 	{
-		while (i < length && str[i] == ' ')
+		if (str[i] == ' ')
 		{
 			i++;
+			continue;
 		}
-		if (i < length && str[i] != ' ')
+		word_count++;
+		while (str[i] != ' ' && str[i] != '\0')
 		{
-			word_count++;
-			while (i < length && str[i] != ' ')
-			{
-				i++;
-			}
+			i++;
 		}
 	}
 	char *words = (char *)malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	i = 0;
 	int word_index = 0;
-	while (i < length)
+
+	while (str[i] != '\0')
 	{
-		while (i < length && str[i] == ' ')
+		if (str[i] == ' ')
 		{
 			i++;
+			continue;
 		}
-		if (i < length && str[i] != ' ')
+
+		j = i;
+		while (str[j] != ' ' && str[j] != '\0')
 		{
-			j = i;
-			while (j < length && str[j] != ' ')
-			{
-				j++;
-			}
-			words[word_index] = (char *)malloc((j - i + 1) * sizeof(char));
-			if (words[word_index] == NULL)
-			{
-				for (int k = 0; k < word_index; k++)
-				{
-				free(words[k]);
-                		}
-				free(words);
-				return NULL;
-			}	
-			strncpy(words[word_index], str + i, j - i);
-			words[word_index][j - i] = '\0';
-			word_index++;	
-			i = j;
+			j++;
 		}
+		words[word_index] = (char *)malloc((j - i + 1) * sizeof(char));
+		if (words[word_index] == NULL)
+		{
+			for (int k = 0; k < word_index; k++)
+			{
+				free(words[k]);
+			}
+			free(words);
+			return (NULL);
+		}
+		strncpy(words[word_index], str + i, j - i);
+		words[word_index][j - i] = '\0';
+		word_index++;
+		i = j;
 	}
 	words[word_index] = NULL;
-	return words;
+	return (words);
 }
