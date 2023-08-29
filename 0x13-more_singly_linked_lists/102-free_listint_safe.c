3b04321ed6d77f646b4de5ba96f5556f379f274d
Walex101019 <ibietanwasiu94@gmail.com>
2023-08-29 16:06:36 -0700

    changes

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -49,12 +49,18 @@ size_t free_listint_safe(listint_t **h)
 	return (count);
 }
 
+/*
+* find_listint_loop - finds the loop in a linked list
+* @head: pointer to the head of the list
+*
+* Return: the address of the node where the loop starts
+*/
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
+
 	if (head == NULL)
 		return (NULL);
-
 	slow = fast = head;
 	while (slow && fast && fast->next)
 	{
